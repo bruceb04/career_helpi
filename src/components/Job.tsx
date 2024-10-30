d37c42288afdd08b34b7474b776b90113b0e41c6
Bruce Bermel <brbe@udel.edu>
2024-10-30 11:21:08 -0400

    Added Styling for results page + job template

--- src/components/Job.tsx
+++ src/components/Job.tsx
@@ -5,7 +5,7 @@ type Props = {title: string, match: number, salary: string, desc: string};
 
 export default function Job({title, match, salary, desc}: Props) {
   return (
-    <div className="job-list-item">
+    <div className="job-item">
       <div className="job-name">{title}</div>
       <div className="job-description">{desc}</div>
       <div className="job-salary">Salary: {salary}</div>
