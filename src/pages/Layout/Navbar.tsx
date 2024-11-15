9d32ca51464a7240b6d996a9015bb8c9e32b98ab
Matthew Kudler <mkudler@udel.edu>
2024-11-15 13:18:33 -0500

    Updating buttons for accuracy

--- src/pages/Layout/Navbar.tsx
+++ src/pages/Layout/Navbar.tsx
@@ -4,8 +4,8 @@ export function Navbar(){
     return (
         <>
         <Link to="/"><button>Home</button></Link>
-        <Link to="/basicquestions"><button>Basic Questions</button></Link>
-        <Link to="/detailedquestions"><button>Detailed Questions</button></Link>
+        <Link to="/basicquestions"><button>Basic Quiz</button></Link>
+        <Link to="/detailedquestions"><button>Detailed Quiz</button></Link>
         </>
     )
 }
